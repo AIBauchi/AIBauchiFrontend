9e3762044d84334fc2d8fadd58bd3eadf0061672
Matthew Wisdom <matthewwisdom11@gmail.com>
2022-10-31 20:13:49 +0100

    Add comment feature

---
+++ components/Comment.vue
@@ -0,0 +1,27 @@
+<template lang="">
+    <article class="cursor-pointer mb-3 pt-5 hover:bg-yellow-200 hover:bg-opacity-40 px-2 pb-2 rounded-sm cursor-pointer">
+        <h3 class="text-left pl-3 text-yellow-600 font-bold">{{name}}</h3>
+        <div class="pb-1 mt-2 break-all text-left pl-6 text-sm">
+            {{content}}
+        </div>
+        <div class="grid grid-cols-4 text-sm mt-3">
+            <div class="grid grid-cols-4 md:grid-cols-7"><i class="text-sm material-icons">access_time</i> <time class="text-yellow-300 text-sm inline-block my-auto">{{$store.state.states.to_relative_days(updated.split(".")[0])}}</time></div>
+            <div class="text-yellow-500 hover:text-yellow-300" v-if="is_auth" @click="$emit('wanna-reply')"> reply </div>
+            <div class="text-yellow-500 hover:text-yellow-300" v-if="is_auth.id === id" @click="$emit('delete')"> delete </div>
+        </div>
+    </article>
+</template>
+<script>
+export default {
+    props: [
+        "content",
+        "updated",
+        "name",
+        "is_auth",
+        "id"
+    ]
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ components/Comments.vue
@@ -0,0 +1,96 @@
+<template>
+    <section class="text-white px-2 text-center min-h-36 pb-2 m-auto">
+      <div @click="show_modal=false" class="modal bg-yellow-900 bg-opacity-20 w-screen h-screen" v-if="show_modal">
+        <div @click.stop="" class="w-full md:w-2/5 h-3/4 md:h-4/5 pt-20 bg-gray-700 text-yellow-500">
+          <h1 class="text-2xl font-bold">Are you sure you want to delete this comment?</h1>
+          <div class="w-full text-center text-red-500 mt-4">{{error}}</div>
+          <div class="mt-12">
+            <button @click="finalDelete" class="hover:bg-red-300 bg-red-600 px-4 py-2 text-center rounded">Yes, I'm sure</button>
+            <button @click="show_modal=false" class="hover:bg-gray-500 bg-gray-900 px-4 py-2 text-center rounded">I've Changed My Mind</button>
+          </div>
+        </div>
+      </div>
+        <h2 class="font-black text-xl my-4">Comments</h2>
+        <p class="text-center text-lg text-yellow-500" v-if="!comments.length">No comments</p>
+        <div class="float-left border-l-2 border-yellow-500 ml-2" v-for="comment in comments" :key="comment.id">
+          <Comment @delete="deleteComm(comment.id, comment.author.id)" :id="comment.author.id" @wanna-reply="makeComment(comment.id, comment.author.name)" class="bg-gray-800 ml-2 md:w-4/5" :content="comment.content" :name="comment.author.name" :is_auth="signedIn" :updated="comment.updatedAt"/>
+          <div class="pl-16 ml-8 border-l-2 border-yellow-500">
+            <Comment :id="child.author.id" @delete="deleteComm(child.id, child.author.id)" @wanna-reply="makeComment(comment.id, comment.author.name)" :key="child.id" v-for="child in comment.children" class="bg-gray-800 ml-2 md:w-4/5" :content="child.content" :name="child.author.name" :is_auth="signedIn" :updated="child.updatedAt"/>
+          </div>
+        </div>
+    </section>
+</template>
+
+<script>
+  export default {
+    name: 'Comments',
+    props: [
+      "id"
+    ],
+    mounted() {
+      this.loadComments(this.page)
+    },
+    methods: {
+      loadComments() {
+        let url = `${this.$store.state.states.base_comments_url+this.id}`
+        this.$axios.$get(url).then((data)=>{
+          this.comments = data
+        })
+      },
+      makeComment(id, name){
+        if (id){
+          document.querySelector("textarea").scrollIntoView({
+            behavior: 'auto',
+            block: 'center',
+            inline: 'center'
+        })
+        this.$emit('replying', {id, name})
+        }
+      },
+      deleteComm(id, authorId){
+        this.error = ""
+        this.delete_target = {id, authorId}
+        this.show_modal = true
+      },
+      finalDelete(){
+        let {id, authorId} = this.delete_target
+        let url = `${this.$store.state.states.base_comments_url+this.id}/comment/${id}?authorId=${authorId}`
+        this.$axios.$get(url).then((data)=>{
+          this.comments = data
+          this.show_modal = false
+        }).catch(()=>{
+          this.error = "Could not delete"
+        })
+      }
+    },
+    computed: {
+        signedIn(){
+            let data = JSON.parse(localStorage.getItem("userData"))
+            return data?data:false
+        }
+    },
+    data() {
+      return {
+        comments: [],
+        page: 1,
+        show_modal: false,
+        delete_target: null,
+        id: this.$route.params.id,
+        error: ""
+      }
+    },
+  }
+</script>
+
+<style scoped>
+    a {
+        @apply hover:bg-gray-700 py-3
+    }
+
+    .modal {
+      z-index: 55555 !important;
+      @apply w-screen h-screen bg-opacity-5;
+      @apply bg-gray-600 fixed top-0 z-50	left-0 px-2;
+      @apply flex justify-center items-center;
+    }
+</style>
