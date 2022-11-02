64b09a35d2bf1e08c98fe2ef56afca9262350ce4
Matthew Wisdom <mattwisdom11@gmail.com>
2022-11-02 03:12:59 +0100

    Update signup form error message

--- pages/signup.vue
+++ pages/signup.vue
@@ -7,22 +7,22 @@
                 <legend class="text-3xl mb-5 font-bold text-yellow-600">Sign Up</legend>
                 <div class="my-4">
                     <h1 class="text-left font-bold mb-2 font-montserrat">Name</h1>
-                    <input type="text" v-model="name" class="text-sm outline-none pb-2 w-full border-yellow-400 bg-transparent border-b hover:border-blue-700 focus:border-blue-700">
+                    <input type="text" v-model="formdata.name" class="text-sm outline-none pb-2 w-full border-yellow-400 bg-transparent border-b hover:border-blue-700 focus:border-blue-700">
                 </div>
                 <div class="my-4">
                     <h1 class="text-left font-bold mb-2 font-montserrat">Email</h1>
-                    <input type="email" v-model="email" class=" outline-none pb-2 w-full border-yellow-400 border-b bg-transparent hover:border-blue-700 focus:border-blue-700">
+                    <input type="email" v-model="formdata.email" class=" outline-none pb-2 w-full border-yellow-400 border-b bg-transparent hover:border-blue-700 focus:border-blue-700">
                 </div>
                 <div class="my-4">
                     <h1 class="text-left font-bold mb-2 font-montserrat">Password</h1>
-                    <input type="password" v-model="password" class="text-sm outline-none pb-2 w-full border-yellow-400 bg-transparent border-b hover:border-blue-700 focus:border-blue-700">
+                    <input type="password" v-model="formdata.password" class="text-sm outline-none pb-2 w-full border-yellow-400 bg-transparent border-b hover:border-blue-700 focus:border-blue-700">
                 </div>
                 <div class="my-4">
                     <h1 class="text-left font-bold mb-2 font-montserrat">Username</h1>
-                    <input type="text" v-model="username" class="text-sm outline-none pb-2 w-full border-yellow-400 border-b hover:border-blue-700 focus:border-blue-700 bg-transparent">
+                    <input type="text" v-model="formdata.username" class="text-sm outline-none pb-2 w-full border-yellow-400 border-b hover:border-blue-700 focus:border-blue-700 bg-transparent">
                 </div>
                 
-                <button type="submit" :disabled="name.length < 4 || password.length < 6 || username.length < 3" class="bg-yellow-600 hover:bg-yellow-500 py-3 px-4 mt-4 mb-3 text-black">
+                <button type="submit" :disabled="formdata.name.length < 4 || formdata.password.length < 6 || formdata.username.length < 3" class="bg-yellow-600 hover:bg-yellow-500 py-3 px-4 mt-4 mb-3 text-black">
                     Sign Up
                 </button>
                 <p class="text-xs mb-4">Already have an account? <nuxt-link class="text-yellow-500" to="/login">Sign In</nuxt-link></p>
@@ -37,24 +37,37 @@ export default {
   name: 'SignUpPage',
   data(){
     return {
-        name: '',
-        email: '',
-        password: '',
-        username: '',
+        formdata: {
+            name: '',
+            email: '',
+            password: '',
+            username: ''
+        },
         error: false,
         errorMsg: 'An error occurred'
     }
   },
+  watch: {
+    formdata(newdata){
+            if (newdata.name.length < 4){
+                this.error = true
+                this.errorMsg = "Ensure name is atleast 4 characters long"
+            } else if ( newdata.password.length < 6){
+                this.error = true
+                this.errorMsg = "Ensure password is atleast 6 characters long"
+
+            } else if (newdata.username.length < 3){
+                this.error = true
+                this.errorMsg = "Ensure username is atleast 3 characters long"
+            }
+
+        }
+  }
   methods: {
         async register(e) {
             try {
                 e.preventDefault()
-                await this.$axios.post(`${this.$store.state.states.base_host}/api/auth/local/register`, {
-                name: this.name,
-                password: this.password,
-                email: this.email,
-                username: this.username
-                })
+                await this.$axios.post(`${this.$store.state.states.base_host}/api/auth/local/register`, this.formdata)
                 let next = this.$route.query.next
                 if (next && next[0] === "/"){
                     window.location.href = next
@@ -65,8 +78,9 @@ export default {
             } catch(e) {
                 this.error = true
                 this.email = ''
+                this.errorMsg = "Signup Failed"
             } 
-        }
+        },
     }
 }
 </script>
